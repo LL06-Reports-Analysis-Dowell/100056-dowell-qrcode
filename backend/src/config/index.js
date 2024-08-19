6e49c461a20de87de910349d8fa8d7d44f059c34
Manish <mdashsharma95@gmail.com>
2024-08-20 03:55:26 +0530

    Fix: Docker compose and mongdb URI secured password

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -4,17 +4,18 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT || 5000,
-    MONGO_DB_URI: process.env.MONGO_DB_URI || "mongodb://mongo:27017/dowellqrcodegenerator",
-    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET || "q1e2r3s4",
-    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY || "10d",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "7d",
-    JWT_SECRET: process.env.JWT_SECRET || "Q1E2r3S4",
-    redisHost: process.env.REDIS_HOST || 'cache',
-    redisPort: process.env.REDIS_PORT || 6379,
-    redisPassword: process.env.REDIS_PASSWORD || 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81'
+    MONGO_DB_URI: process.env.MONGO_DB_URI,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET ,
+    REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY ,
+    JWT_EXPIRY: process.env.JWT_EXPIRY ,
+    JWT_SECRET: process.env.JWT_SECRET,
+    redisHost: process.env.REDIS_HOST ,
+    redisPort: process.env.REDIS_PORT ,
+    redisPassword: process.env.REDIS_PASSWORD 
 };
 
 
+console.log(config);
 
 export default config;
 
